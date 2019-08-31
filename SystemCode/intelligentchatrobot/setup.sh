6027617a7e7aa91dda086efc0f0b84b45b55791d
harry0916 <harrymwz@126.com>
2019-08-31 15:00:55 +0800

    do final submission preparation

--- SystemCode/intelligentchatrobot/setup.sh
+++ SystemCode/intelligentchatrobot/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-conda create -n safari
-conda activate safari
+conda create -n safari --yes
+conda activate safari --yes
 
 pip install -r requirements.txt
 python -m spacy download en_core_web_md
